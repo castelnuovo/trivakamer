ec7b98d67558df9c007a7cfc1f1a2c100926678b
Luca Castelnuovo <luca@castelnuovo.xyz>
2021-05-02 19:21:07 +0200

    integrated first version of trivakamer frontend

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use CQ\Controllers\Controller;
+use CQ\Helpers\AuthHelper;
 use CQ\Response\HtmlResponse;
 use CQ\Response\Respond;
 
@@ -15,8 +16,24 @@ final class UserController extends Controller
      */
     public function dashboard(): HtmlResponse
     {
+        $msg = match ($this->requestHelper->getQueryParam('msg')) {
+            'error' => 'Please try again',
+            'logout' => 'You have been logged out',
+            'not_verified' => 'Please verify your email',
+            'not_registered' => 'Please create an account',
+            default => ''
+        };
+
         return Respond::twig(
-            view: 'dashboard.twig'
+            view: 'index.twig',
+            parameters: [
+                'message' => $msg,
+                'logged_in' => AuthHelper::isValid(),
+            ]
         );
+
+        // return Respond::twig(
+        //     view: 'dashboard.twig'
+        // );
     }
 }
