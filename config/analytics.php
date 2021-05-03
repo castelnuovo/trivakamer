ed834b145037607ee6aefb7825917b7f33d57276
Luca Castelnuovo <luca@castelnuovo.xyz>
2021-05-03 12:51:48 +0200

    enable analytics

--- config/analytics.php
+++ config/analytics.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'enabled' => false,
-    'domainId' => '', // get from stats.castelnuovo.xyz
+    'enabled' => true,
+    'domainId' => '8ca5a20f-a9b7-4e60-b0f2-aebb1d7a6a74', // get from stats.castelnuovo.xyz
 ];
