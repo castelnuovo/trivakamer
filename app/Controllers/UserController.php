7e3f4f0a2ae22c9a72feefc1b3b4d7762b89bfb2
Luca Castelnuovo <luca@castelnuovo.xyz>
2021-05-18 00:47:00 +0200

    show dashboard on route

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -16,24 +16,11 @@ final class UserController extends Controller
      */
     public function dashboard(): HtmlResponse
     {
-        $msg = match ($this->requestHelper->getQueryParam('msg')) {
-            'error' => 'Please try again',
-            'logout' => 'You have been logged out',
-            'not_verified' => 'Please verify your email',
-            'not_registered' => 'Please create an account',
-            default => ''
-        };
-
         return Respond::twig(
-            view: 'index.twig',
+            view: 'dashboard.twig',
             parameters: [
-                'message' => $msg,
                 'logged_in' => AuthHelper::isValid(),
             ]
         );
-
-        // return Respond::twig(
-        //     view: 'dashboard.twig'
-        // );
     }
 }
